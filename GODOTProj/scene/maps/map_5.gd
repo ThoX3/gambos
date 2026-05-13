f3134cf976f3cf08707106f5d5ddf62a2e2e9511
hippolytecosserat <hippolytecosserat@gmail.com>
2026-05-13 16:37:25 -0400

    Feat: map files

--- GODOTProj/scene/maps/map_5.gd
+++ GODOTProj/scene/maps/map_5.gd
@@ -1,11 +1,11 @@
 extends BaseMap
 
-
 func _ready() -> void:
 	super._ready()
 
 func _physics_process(_delta: float) -> void:
 	super._physics_process(_delta)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:

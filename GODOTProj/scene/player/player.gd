e61038a7176b092c3890383aa83d854078dae2b1
hippolytecosserat <hippolytecosserat@gmail.com>
2026-05-13 15:06:40 -0400

    Feat: Main & Map1 scenes

--- GODOTProj/scene/player/player.gd
+++ GODOTProj/scene/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var speed = 300.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if direction:
+		velocity = direction * speed
+		
+		if 
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+
+	move_and_slide()

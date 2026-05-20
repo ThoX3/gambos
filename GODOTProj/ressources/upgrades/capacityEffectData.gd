a50dd80699ae2aefb40734466b7e02152aa6a898
Emeline <emeline.gehanno@gmail.com>
2026-05-20 17:40:29 -0400

    Feat: Create gabarit for upgrades

---
+++ GODOTProj/ressources/upgrades/capacityEffectData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name capacityEffectData
+
+enum TargetCapacityEffect {PLAYER_HEALTH, PLAYER_SPEED, PLAYER_DAMAGE, PLAYER_ATTACK_SPEED, PLAYER_ATTACK_RANGE, PLAYER_COLLECT_RANGE}
+
+@export var targetCapacity: TargetCapacityEffect
+@export var value: int
---
+++ GODOTProj/ressources/upgrades/skillEffectData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name skillEffectData
+
+enum TargetSkillEffect {DAMAGE, RANGE}
+
+@export var skill_target: TargetSkillEffect
+@export var value: int

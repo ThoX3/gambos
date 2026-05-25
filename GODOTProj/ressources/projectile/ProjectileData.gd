1ef46d4cc132ebc7c6d835e95457be33e78ad0ce
Florian Heme <florian.heme@laposte.net>
2026-05-25 17:11:42 -0400

    equilibrage fire rate

--- GODOTProj/ressources/projectile/ProjectileData.gd
+++ GODOTProj/ressources/projectile/ProjectileData.gd
@@ -2,8 +2,8 @@ extends Resource
 class_name ProjectileData
 
 @export_group("Combat")
-@export var damage: int = 2
+@export var damage: int = 1
 @export var speed: float = 400.0
 @export var range: float = 500.0      # portée en pixels
-@export var fire_rate: float = 1.0    # tirs par seconde
+@export var fire_rate: float = 0.5    # tirs par seconde
 @export var projectile_count: int = 1

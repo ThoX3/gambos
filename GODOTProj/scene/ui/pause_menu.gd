9495c868dce1932edf064e5df3c5490ceb984578
Emeline <emeline.gehanno@gmail.com>
2026-05-30 16:40:09 -0400

    Feat: Display stats in pause menu

--- GODOTProj/scene/ui/pause_menu.gd
+++ GODOTProj/scene/ui/pause_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const STATS_FONT = preload("res://assets/fonts/depixel/DePixelBreit.ttf")
+
 func _ready() -> void:
 	hide()
 	
@@ -12,4 +14,18 @@ func toggle_pause():
 	get_tree().paused = new_pause_state
 	%LayerPause.visible = new_pause_state
 	if new_pause_state:
+		update_stats_display()
 		%Resume.grab_focus()
+		
+func update_stats_display():
+	for child in %PlayerStats.get_children():
+		child.queue_free()
+	var player = get_tree().get_first_node_in_group("Player")
+	if player and player.has_method("get_player_stats"):
+		var stats = player.get_player_stats()
+		for category in stats:
+			var label = Label.new()
+			label.text = category + str(stats[category])
+			label.add_theme_font_override("font", STATS_FONT)
+			%PlayerStats.add_child(label)
+		

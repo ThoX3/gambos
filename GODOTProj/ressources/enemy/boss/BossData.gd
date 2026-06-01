44ddd07b743028aeacb3460c05b13270d2c70909
Antony <135183318+ThoX3@users.noreply.github.com>
2026-06-01 19:19:35 -0400

    Revert "Gam 31 comportement et patterns du boss"

--- GODOTProj/ressources/enemy/boss/BossData.gd
+++ GODOTProj/ressources/enemy/boss/BossData.gd
@@ -3,7 +3,3 @@ class_name BossData
 
 @export_group("Attack Pattern")
 @export var attack_scripts: Array[Script] = []
-
-@export_group("UI")
-@export var scene_transition: PackedScene
-@export var boss_ui_scene: PackedScene

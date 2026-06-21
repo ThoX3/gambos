3f934f3d02388167d10d26acd57cb4475e8da436
Florian Heme <florian.heme@laposte.net>
2026-06-21 12:02:46 -0400

    sable projectile rate

--- GODOTProj/ressources/projectile/ProjectileDataSable.gd
+++ GODOTProj/ressources/projectile/ProjectileDataSable.gd
@@ -7,4 +7,4 @@ class_name ProjectileDataSable
 @export var speed: float = 600.0
 @export var range: float = 700.0
 #@export var cooldown: float = 0.8
-@export var cadence_ratio: float = 0.5
+@export var cadence_ratio: float = 2
